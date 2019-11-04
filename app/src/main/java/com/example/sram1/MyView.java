ae09dd27864c3fd2850a5fbf0e8e2fdb3d9f2cba
Шизофреник <ogyrcka@gmail.com>
2019-11-04 17:49:39 +0300

    1.8.11

--- app/src/main/java/com/example/sram1/MyView.java
+++ app/src/main/java/com/example/sram1/MyView.java
@@ -6,92 +6,76 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
-import android.os.CountDownTimer;
+
 import static android.graphics.Color.rgb;
 
 public class MyView extends View {
-
-
-
-    int N = 15;
-    int[] l = new int [N];
-    double x0, y0;
-    double[] x = new double [N];
-    double[] y = new double [N];
-    double g = 9.832f, pi = Math.PI;
-    double[] w = new double[N];
-    double fi0;
-    double[] fi = new double[N];
-    int t = 0, deltaT = 1;
-
-    void makePendulum()
-    {
-        fi0 = pi/4;
-
-        int l_min = 100;
-        for (int i = 0; i<N; i++)
-        {
-            l[i] = l_min;
-            l_min += 50;
-
-            w[i] = Math.sqrt(g/l[i]);
-        }
-    }
-    void movePendulum()
-    {
-        t += deltaT;
-
-        for (int i = 0; i<N; i++)
-        {
-            fi[i] = fi0 * Math.cos(w[i] * t);
-            x[i] = l[i]*Math.sin(fi[i]);
-            y[i] = l[i]*Math.cos(fi[i]);
-        }
-    }
-
-
     public MyView(Context context) {
-        super(context);
-        makePendulum();
-        MyTimer timer = new MyTimer();
-        timer.start();
+        super(context);for (int i = 0; i < N; i++){
+            x[i] = (float)(Math.random() );
+            y[i] = (float)(Math.random() );
+            vx[i] = (float)(Math.random()*10 );
+            vy[i] = (float)(Math.random()*10 );
+            rr[i]=(float)(Math.random() );
+            k[i]=0;
+            v[i]=0;
+            g[i]=0;
+            cc[i]=7;
+            zz[i]=12;
+            qq[i]=19;
+            r[i]=20;
+            c[i]=7;
+            z[i]=12;
+            q[i]=19;
+        }
     }
 
+    Paint paint = new Paint();
+    int N = 50;
 
+    float[] x  = new float[N];
+    float[] y  = new float[N];
+    float[] vx = new float[N];
+    float[] vy = new float[N];
+    float[] k = new float[N];
+    float[] v = new float[N];
+    float[] r = new float[N];
+    float[] rr=new float[N];
+    float[] g = new float[N];
+    float[] cc = new float[N];
+    float[] zz = new float[N];
+    float[] qq = new float[N];
+    float[] c = new float[N];
+    float[] z = new float[N];
+    float[] q = new float[N];
+    int re=255,gr=0,bl=0;
     @Override
 
     protected void onDraw(Canvas canvas) {
-        x0 = getWidth()/2;
-        y0 = getHeight()/4;
-        Paint paint = new Paint();
-        canvas.drawCircle((float) x0, (float) y0, 10, paint);
-        for (int i = 0; i<N; i++)
-        {
-            paint.setColor(Color.BLUE);
-            canvas.drawLine((float)x0, (float)y0, (float)(x[i] + x0), (float)(y[i]+ y0), paint);
-            paint.setColor(Color.RED);
-            canvas.drawCircle((float)(x[i] + x0), (float)(y[i] + y0), 20, paint);
-        }
-    }
 
-    void nextFrame()
-    {
-        movePendulum();
+        for (int i = 0; i < N; i++) {
+            paint.setColor(Color.rgb(re,gr,bl));
+            canvas.drawCircle(x[i], y[i], r[i], paint);
+        }
+        // готовим массивы x и у для следущего кадра
+        for (int i = 0; i < N; i++) {
+            if(x[i]<this.getWidth()&&k[i]==0){x[i] += vx[i];}else {k[i]=1;;while(x[i]>0){x[i] -= vx[i];break;}if(x[i]<5){k[i]=0;}}
+            if(y[i]<this.getHeight()&&v[i]==0){y[i] += vy[i];}else {v[i]=1;;while(y[i]>0){y[i] -= vy[i];break;}if(y[i]<5){v[i]=0;}}
+            if(r[i]<50&&g[i]==0){r[i] += rr[i];}else {g[i]=1;;while(r[i]>0){r[i] -= rr[i];break;}if(r[i]<5){g[i]=0;}}
+
+
+    }
+          if(re==255&&gr==0&&bl!=255){bl+=1;}
+            else if(re==0&&gr!=255&&bl==255){gr+=1;}
+            else if (re==0&&gr==255&&bl!=0){bl-=1;}
+            else if (re!=255&&gr==255&&bl==0){re+=1;}
+            else if (re==255&&gr!=0&&bl==0){gr-=1;}
+            else if(re!=0&&gr==0&&bl==255){re-=1;}
+
+
+
+
+
         invalidate();
-    }
-
-    class MyTimer extends CountDownTimer
-    {
-        MyTimer()
-        {
-            super(100000, 100);
-        }
-        @Override
-        public void onTick(long millisUntilFinished) {
-            nextFrame();
-        }
-        @Override
-        public void onFinish() {
-        }
     }
 }
